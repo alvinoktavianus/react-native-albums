930a517fed1edfcf2709ba00e434a03d1c85f8a2
Alvin Oktavianus <agentsam1995@gmail.com>
2017-06-28 10:57:37 +0700

    Remove default code

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alvinoktavianus on 6/28/2017.
+ */
+
+import React, {Component} from 'react'
+import {Text} from 'react-native'
+
+class Header extends Component {
+  render() {
+    return <Text>Albums!</Text>
+  }
+}
+
+export default Header
